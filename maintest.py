71f8e6c09ce96f75ee3eae7e65b901623e2f6c57
shyam kumar <shyam.cs.engg@gmail.com>
2021-04-14 14:22:39 +0530

    https://www.hackerrank.com/challenges/compare-the-triplets/problem

--- maintest.py
+++ maintest.py
@@ -1,17 +1,16 @@
-arr = [-4, 3, -9, 0, 4, 1]
-plus = 0
-minus = 0
-zero = 0
-lenarr = len(arr)
-for i in arr:
-    if i > 0:
-        plus += 1
-    if i < 0:
-        minus += 1
-    if i == 0:
-        zero += 1
+import math
+import os
+import random
+import re
+import sys
 
-print(str.format('{0:.6f}', plus / lenarr))
-print(str.format('{0:.6f}', minus / lenarr))
-print(str.format('{0:.6f}', zero / lenarr))
+# Complete the staircase function below.
+def staircase(n):
+    for i in range(1,n+1):        
+        s=n-i
+        st=i
+        print(s*" "+i*"#")
+if __name__ == '__main__':
+    n = int(input())
 
+    staircase(n)
