578fdb70ff26ee97dce45a7f22e5e0fb1a88127b
shyam kumar <shyam.cs.engg@gmail.com>
2021-04-14 23:35:46 +0530

    https://www.hackerrank.com/challenges/time-conversion/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen

--- maintest.py
+++ maintest.py
@@ -1,4 +1,25 @@
-a=[3,2,1,3]
-max_arr=max(a)
-print(max_arr)
-print(a.count(max_arr))
+s = "08:05:45AM"
+s =  "04:59:59AM"
+a = s[-2:] # AM or PM
+le = len(s)
+s = s[0:le - 2]
+tmsplt = s.split(":")
+hr = int(tmsplt[0])
+min = tmsplt[1]
+sec = tmsplt[2]
+if a == "PM":
+    if hr!=12:
+        hr1=12+hr
+        print(str(hr1)+":"+str(min)+":"+str(sec))
+    else:
+        print(str(hr)+":"+str(min)+":"+str(sec))
+
+else:
+    if hr==12:
+        hr1 = 12 - hr
+        print(str(hr1).zfill(2) + ":" + str(min) + ":" + str(sec))
+    else:
+        print(str(hr).zfill(2) + ":" + str(min) + ":" + str(sec))
+
+
+
