6b920393d1629e9741a8ab94d39d1fb5c54c3008
shyam kumar <shyam.cs.engg@gmail.com>
2021-04-14 20:42:12 +0530

    arr=[1,2,3,4,5], get the min and max value except one

--- maintest.py
+++ maintest.py
@@ -1,16 +1,21 @@
-import math
-import os
-import random
-import re
-import sys
+arr=[396285104,573261094,759641832,819230764,364801279]
+#arr=[140638725,436257910,953274816,734065819,362748590]
+#arr=[1,2,3,4,5]
 
-# Complete the staircase function below.
-def staircase(n):
-    for i in range(1,n+1):        
-        s=n-i
-        st=i
-        print(s*" "+i*"#")
-if __name__ == '__main__':
-    n = int(input())
+min=0
+max=0
+flag =1
+for i in range(len(arr)):
+    arsum = sum(arr[:i] + arr[i + 1:])
+    if ((arsum < max) & flag):
+        min=arsum
+        flag=0
+    if ((flag==0) & (arsum <min)):
+        min = arsum
 
-    staircase(n)
+    if max < arsum:
+        max = arsum
+
+
+print(min)
+print(max)
