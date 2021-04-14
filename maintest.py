0aea391f8864c720821d0e48d885139b487b896e
shyam kumar <shyam.cs.engg@gmail.com>
2021-04-14 20:47:20 +0530

    https://www.hackerrank.com/challenges/birthday-cake-candles/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen

--- maintest.py
+++ maintest.py
@@ -1,21 +1,4 @@
-arr=[396285104,573261094,759641832,819230764,364801279]
-#arr=[140638725,436257910,953274816,734065819,362748590]
-#arr=[1,2,3,4,5]
-
-min=0
-max=0
-flag =1
-for i in range(len(arr)):
-    arsum = sum(arr[:i] + arr[i + 1:])
-    if ((arsum < max) & flag):
-        min=arsum
-        flag=0
-    if ((flag==0) & (arsum <min)):
-        min = arsum
-
-    if max < arsum:
-        max = arsum
-
-
-print(min)
-print(max)
+a=[3,2,1,3]
+max_arr=max(a)
+print(max_arr)
+print(a.count(max_arr))
