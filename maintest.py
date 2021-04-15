12490fa6688207986ff6e853d5f0846eddec6966
shyam kumar <shyam.cs.engg@gmail.com>
2021-04-15 09:50:27 +0530

    https://www.hackerrank.com/challenges/hackerrank-in-a-string/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen

--- maintest.py
+++ maintest.py
@@ -1,25 +1,13 @@
-s = "08:05:45AM"
-s =  "04:59:59AM"
-a = s[-2:] # AM or PM
-le = len(s)
-s = s[0:le - 2]
-tmsplt = s.split(":")
-hr = int(tmsplt[0])
-min = tmsplt[1]
-sec = tmsplt[2]
-if a == "PM":
-    if hr!=12:
-        hr1=12+hr
-        print(str(hr1)+":"+str(min)+":"+str(sec))
-    else:
-        print(str(hr)+":"+str(min)+":"+str(sec))
+s = "hacakaeararanaka"
+match = "hackerrank"
+i = 0
 
+for x in s:
+    if i<= len(match)-1:
+        if (match[i] == x):
+            i = i + 1
+
+if i != len(match):
+    print("No")
 else:
-    if hr==12:
-        hr1 = 12 - hr
-        print(str(hr1).zfill(2) + ":" + str(min) + ":" + str(sec))
-    else:
-        print(str(hr).zfill(2) + ":" + str(min) + ":" + str(sec))
-
-
-
+    print("Yes")
