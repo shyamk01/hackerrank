3a41e282db74b7a4d0910c03ff596ad6ccd53c85
shyam kumar <shyam.cs.engg@gmail.com>
2021-04-16 10:00:48 +0530

    https://www.hackerrank.com/challenges/weighted-uniform-string/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen

--- maintest.py
+++ maintest.py
@@ -1,11 +1,26 @@
-import re
 from string import ascii_lowercase
+from collections import Counter
+from itertools import groupby
 
-s="We promptly judged antique ivory buckles for the next prize"
-s=s.lower()
-alphabet=sorted(set(ascii_lowercase))
-s1=re.findall(r'\w',s)
-if(set(alphabet).issubset(set(s1))):
-    print("Pangrams")
-else:
-    print("Not Pangrams")
+s = "abccddde"
+queries=[5,9,7,8,12,5]
+import  time
+start= round(time.time() * 1000)
+alphabet = sorted(set(ascii_lowercase))
+enlist = enumerate(alphabet, 1)
+alphdict=dict((j,i) for i,j in enlist)
+ars=[]
+for _, j in groupby(s):
+    sumi=0
+    for i in list(j):
+        sumi =sumi+ int(alphdict[i])
+        ars.append(sumi)
+for a in queries:
+    if a in ars:
+        print("Yes")
+    else:
+        print("No")
+
+end =round(time.time() * 1000)
+
+print("the time taken to execute the program{f}", end-start)
