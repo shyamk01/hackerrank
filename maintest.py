a63ba10cd739a8667b4dcc36f3cddbc0f7b23edc
shyam kumar <shyam.cs.engg@gmail.com>
2021-04-18 11:31:22 +0530

    https://www.hackerrank.com/challenges/funny-string/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen

--- maintest.py
+++ maintest.py
@@ -1,27 +1,17 @@
-a = "11111111111111111111111111111111"
-incr = 1
-flag = 0
-arr = []
-for i in range(1, len(a)):
-    for j in range(1, len(a) + 2):
-        temp1 = incr * j
-        if a[0:temp1] != "" and len(a[0:incr * j]) == incr:
-            val = a[0:temp1]
-            arr.append(val)
-        break
-    incr = incr + 1
 
-for x in arr:
-    str1 = x
-    for j in range(1, len(a)):
-        str1 += str(int(x) + j)
-        if len(str1) >= len(a):
-            #str1 = str1[0:len(a)]
-            break
-    if str1 == a:
-        print(str1)
-        print("Yes " + x)
-        flag = 1
-        break
-if flag == 0:
-    print("No")
+
+def diffstr(s):
+    arrs = []
+    for i in range(len(s)-1):
+        if i<=len(s):
+            arrs.append(abs(ord(s[i + 1]) - ord(s[i])))
+    return arrs
+inps="bcxz"
+orgstr=diffstr(inps)
+revstr=diffstr(inps[::-1])
+print(orgstr)
+print(revstr)
+if orgstr==revstr:
+    print("Funny")
+else:
+    print("Not Funny")
