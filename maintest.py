aa6fa9876a014b03e56cc82afa8e44731cacd74a
shyam kumar <shyam.cs.engg@gmail.com>
2021-04-19 16:39:40 +0530

    Virus Outbreak (100 Marks) In the Martian land faraway, a new virus has evolved and is attacking the individuals at a fast pace. The scientists have figured out the virus composition, V. The big task is to identify the people who are infected. The sample of N people is taken to check if they are POSITIVE or NEGATIVE. A report is generated which provides the current blood composition B of the person.
    POSITIVE or NEGATIVE ?
    
    If the blood composition of the person is a subsequence of the virus composition V, then the person is identified as POSITIVE otherwise NEGATIVE.
    
    Example:
    
    Virus Composition, V = coronavirus
    
    Blood Composition of the person , B = ravus
    
    The person in question is POSITIVE as B is the subsequence of the V.
    
    The scientists are busy with their research for medicine and request you to build a program which can quickly figure out if the person is POSITIVE or NEGATIVE. They will provide you with the virus composition V and all the people’s current blood composition. Can you help them?
    
    Note: The virus and blood compositions are lowercase alphabet strings.
    
    Input Format
    The first line of the input consists of the virus composition, V
    
    The second line of he input consists of the number of people, N
    
    Next N lines each consist of the blood composition of the ith person, Bi
    
    Constraints
    1<= N <=10
    
    1<= |B|<= |V|<= 10^5
    
    Output Format
    For each person, print POSITIVE or NEGATIVE in a separate line
    
    Sample TestCase 1
    Input
    coronavirus
    3
    abcde
    crnas
    onarous
    Output
    NEGATIVE
    POSITIVE
    NEGATIVE

--- maintest.py
+++ maintest.py
@@ -1,18 +1,50 @@
-import string
-inparr = ['abcdde', 'baccd', 'eeabg']
-#inparr = ['abc', 'abc', 'bc']
-alphabet_string = string. ascii_lowercase
-alphabet_list = list(alphabet_string)
-arr=[]
-for ax in alphabet_list:
-    str=""
-    for x in range(len(inparr)):
-        if ax in inparr[x]:
-            str=str+ax
-        else:
-            str = ""
-            break;
-    if str!="":
-        arr.append(str)
+viruscompo = "coronavirus"
+pone = "abcde"  # NEGATIVE
+#pone="crnas" #POSITIVE
+# pone="onarous" #NEGATIVE
+''' Read input from STDIN. Print your output to STDOUT '''
 
-print(len(arr))
+
+# Use input() to read input from STDIN and use print to write your output to STDOUT
+
+def checkPatienceStatus(viruscompo, inpone):
+    arrstr = []
+    flag = 0
+    for pone in inpone:
+        k = 0
+        str = ""
+        for i in range(len(pone)):
+            for _ in range(len(viruscompo)):
+                if set(pone).issubset(set(viruscompo)):
+                    flag = 1
+                else:
+                    break
+                if k <= len(viruscompo) - 1 and pone[i] == viruscompo[k] and flag == 1:
+                    str = str + pone[i]
+                    break
+                else:
+                    k = k + 1
+
+        if pone == str:
+            arrstr.append("POSITIVE")
+        elif flag == 0:
+            arrstr.append("NEGATIVE")
+
+        else:
+            arrstr.append("NEGATIVE")
+    return arrstr
+
+
+def main():
+    viruscompo = input()
+    n = int(input())
+    arr = []
+    for _ in range(n):
+        arr_item = input()
+        arr.append(arr_item)
+    result = checkPatienceStatus(viruscompo, arr)
+    [print(a) for a in result]
+
+
+main()
+
