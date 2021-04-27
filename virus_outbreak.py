c9e3a97d81fc5a076862131cecf5e6028bcb8783
shyam kumar <shyam.cs.engg@gmail.com>
2021-04-27 20:05:08 +0530

    You are given a string consisting of lowercase letters of the English…
    … alphabet. You must split this string into a minimal number of substrings in such a way that no letter occurs more than once in each substring.
    
    For example, here are some correct splits of the string "abacdec": ('a', 'bac', 'dec'), ('a', bacd', 'ec') and ('ab', 'ac', 'dec').
    Write a function:
    def solution(S)
    that, given a string S of length N, returns the minimum number of substrings into which the string has to be split.
    Examples:
    1. Given 'world', your function should return 1. There is no need to split the string into substrings as all letters occur just once.
    2. Given 'dddd', your function should return 4. The result can be achieved by splitting the string into four substrings ('d', 'd', 'd', 'd').
    3. Given 'cycle', your function should return 2. The result can be achieved by splitting the string into two substrings ('cy', 'cle') or ('c', 'ycle').
    4. Given 'abba', your function should return 2. The result can be achieved by splitting the string into two substrings ('ab', 'ba').
    Write an efficient algorithm for the following assumptions:
    N is an integer within the range 1 to 1,000,000
    string S consists only of lowercase letters

---
+++ virus_outbreak.py
@@ -0,0 +1,45 @@
+viruscompo = "coronavirus"
+pone = "abcde"  # NEGATIVE
+#pone="crnas" #POSITIVE
+# pone="onarous" #NEGATIVE
+''' Read input from STDIN. Print your output to STDOUT '''
+
+
+# Use input() to read input from STDIN and use print to write your output to STDOUT
+
+def checkPatienceStatus(viruscompo, inpone):
+    arrstr = []
+    flag = 0
+    for pone in inpone:
+        k = 0
+        str = ""
+        for i in range(len(pone)):
+            for _ in range(len(viruscompo)):
+                if set(pone).issubset(set(viruscompo)):
+                    flag = 1
+                else:
+                    break
+                if k <= len(viruscompo) - 1 and pone[i] == viruscompo[k] and flag == 1:
+                    str = str + pone[i]
+                    break
+                else:
+                    k = k + 1
+
+        if pone == str:
+            arrstr.append("POSITIVE")
+        elif flag == 0:
+            arrstr.append("NEGATIVE")
+        else:
+            arrstr.append("NEGATIVE")
+    return arrstr
+
+
+def main():
+    viruscompo = input()
+    n = int(input())
+    arr = []
+    for _ in range(n):
+        arr_item = input()
+        arr.append(arr_item)
+    result = checkPatienceStatus(viruscompo, arr)
+    [print(a) for a in result]
