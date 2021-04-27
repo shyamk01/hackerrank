5aa046c1d1af75ae79fec7788fc6bc7a278931a3
shyam kumar <shyam.cs.engg@gmail.com>
2021-04-27 20:27:53 +0530

    https://www.hackerrank.com/challenges/birthday-cake-candles/problem?h%E2%80%A6%E2%80%A6_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen

---
+++ birthday_candle.py
@@ -0,0 +1,7 @@
+'''
+https://www.hackerrank.com/challenges/birthday-cake-candles/problem?h%E2%80%A6%E2%80%A6_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+'''
+a=[3,2,1,3]
+max_arr=max(a)
+print(max_arr)
+print(a.count(max_arr))
