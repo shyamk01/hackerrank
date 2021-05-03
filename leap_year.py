059f3a3142214551f7c9a0d76e9388c5257cf18e
shyam kumar <shyam.cs.engg@gmail.com>
2021-05-03 09:21:09 +0530

    https://www.hackerrank.com/challenges/write-a-function/problem LEAP YEAR

---
+++ leap_year.py
@@ -0,0 +1,12 @@
+def is_leap(year):
+    leap = False
+    if year % 4 == 0:
+        leap=True
+        if year % 400 == 0:
+            leap = True
+        elif (year % 100) == 0:
+            leap = False
+
+    return leap
+
+print(is_leap(2100))
