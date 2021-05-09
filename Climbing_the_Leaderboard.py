e5488cef73b32c159cee25a016d5ac48277977f1
shyam kumar <shyam.cs.engg@gmail.com>
2021-05-09 11:13:20 +0530

    https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem

--- Climbing_the_Leaderboard.py
+++ Climbing_the_Leaderboard.py
@@ -1,41 +1,12 @@
 # climbingLeaderboard
 ranked = [100, 100, 50, 40, 20, 10]
-player = [5, 25, 50, 120]
+players = [5, 25, 50, 120]
+ranks = sorted(list(set(ranked)), reverse=True)
 
-def method_name(k):
-    rank = 1
-    rank_upd = [str(rank) + ":" + str(ranked[0])]
-    for x in range(len(ranked)):
-        if x < len(ranked) - 1:
-            if ranked[x] == ranked[x + 1]:
-                rank_upd.append(str(rank) + ":" + str(ranked[x + 1]))
-                if x+1 == k:
-                    print(rank)
-            else:
-                rank = rank + 1
-                rank_upd.append(str(rank) + ":" + str(ranked[x + 1]))
-                if x+1 == k:
-                    print(rank)
-    if k == 0:
-        print(1)
-    if k != -1:
-        del rank_upd[k:k + 1]
-        del ranked[k:k + 1]
+ranked_list=[]
+for player in players:
+    while ranks and player>=ranks[-1]:
+        ranks.pop()
+    ranked_list.append(len(ranks)+1)
 
-for xp in range(len(player)):
-    k = 0
-    for x in range(len(ranked)):
-        if player[xp] < ranked[x]:
-            k = k + 1
-            continue
-        elif player[xp] > ranked[x]:
-            k = x
-            ranked.insert(x, player[xp])
-            break
-        elif player[xp] == ranked[x]:
-            k = x + 1
-            ranked.insert(x + 1, player[xp])
-            break
-    if k == len(ranked):
-        ranked.insert(k + 1, player[xp])
-    method_name(k)
+print(ranked_list)
