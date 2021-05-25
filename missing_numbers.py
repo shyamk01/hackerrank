9ce321928cfcae1eb614ba149d12fd03d4c59020
shyam kumar <shyam.cs.engg@gmail.com>
2021-05-25 17:38:44 +0530

    https://www.hackerrank.com/challenges/missing-numbers/problem
    Missing Numbers

--- missing_numbers.py
+++ missing_numbers.py
@@ -1,3 +1,4 @@
+# https://www.hackerrank.com/challenges/missing-numbers/problem
 a = [203, 204, 205, 206, 207, 208, 203, 204, 205, 206]
 b = [203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204]
 
