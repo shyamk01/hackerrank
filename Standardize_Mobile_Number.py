606aa0e41c6470558bbc7c6c43f0291d1599abe4
shyam kumar <shyam.cs.engg@gmail.com>
2021-07-11 12:27:38 +0530

    https://www.hackerrank.com/challenges/standardize-mobile-number-using-decorators/problem
    Standardize Mobile Number Using Decorators

--- Standardize_Mobile_Number.py
+++ Standardize_Mobile_Number.py
@@ -1,7 +1,7 @@
 def wrapper(f):
     def fun(l):
-        l=map(int,l[-10:])
-        l=sorted(l)
+        res = [int(x[-10:]) for x in l]
+        l=sorted(res)
         l=map(str,l)
         for a in l:
             print("+91 " + a[-10:-5] + " " + a[-5:])
@@ -13,5 +13,5 @@ def sort_phone(l):
 
 
 if __name__ == '__main__':
-    x = ['09191919191', '9100256236', '+919593621456']
+    x = ['08915625469', '+918756954120']
     sort_phone(x)
