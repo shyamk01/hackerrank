46278c6032c7b8e1e6616c390ed24e3270bd52e0
shyam kumar <shyam.cs.engg@gmail.com>
2021-08-01 14:36:21 +0530

    https://www.hackerrank.com/challenges/crush/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
    Array Manipulation

--- array_manipulations.py
+++ array_manipulations.py
@@ -1,15 +1,20 @@
 n=5
 queries=[[1, 2, 100], [2, 5, 100], [3, 4, 100]]
-a=[0]*n
-t=[0]*n
+a=[0]*(n+1)
+t=[0]*(n+1)
 
 for qa in queries:
-    t1=qa[0]-1
-    t2=qa[1]
-    sm=qa[-1]
-    a[t1:t2] = [sm] * (t2 - t1)
-    for tx in range(t1,t2,1):
-        t[tx]+=a[tx]
+    t1 = qa[0]-1
+    t2 = qa[1]
+    sm = qa[-1]
+    a[t1]+=sm
+    a[t2]+=-sm
+
+max=sum=0
+for i in a:
+    sum+=i
+    if sum>max:
+        max=sum
 
 
-print(max(t))
+print(max)
