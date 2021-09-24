31ab0b17125c967c8d8f34ce56061b29546d86ef
shyam kumar <shyam.cs.engg@gmail.com>
2021-09-24 21:07:20 +0530

    Intermediate work only-- that is not the solutions

--- Organizing_Containers.py
+++ Organizing_Containers.py
@@ -1,15 +1,27 @@
-t=[[0, 2,1], [1,1,1],[2,0,0]]
-x=len(t)
-balldict={}
-for i in range(x):
-    k = []
-    for j in range(x):
-        # print("container: "+str(i))
-        # print("Ball number: "+str(j))
-        # print("How many ball of particular ball number: "+str(t[i][j]))
-        k.append(t[i][j])
-    balldict[i]=k
-print(balldict)
+t = [[0, 2, 1], [1, 1, 1], [2, 0, 0]]
 
-for i,y in balldict.items():
-        print(i,y)
+
+# 0 ->3
+# 1->4
+# 2->2
+
+def matrixswap(i, j):
+    while (t[i][j] != 0) or (t[j][i] != 0) or (t[i + 1][j] != 0) or (t[j + 1][i] != 0):
+        t[i][i] = t[i][i] + 1
+        t[i][j] = t[i][j] - 1
+
+        t[j][i] = t[j][i] - 1
+        t[j][j] = t[j][j] + 1
+
+        print(i, j, t)
+        if (t[i][j] == 0) or (t[j][i] == 0) or (t[i + 1][j] == 0) or (t[j + 1][i] == 0):
+            break
+
+
+for i in range(len(t) - 1):
+    for j in range(len(t) - 1):
+        if i != j + 1:
+            print(i, j + 1)
+            matrixswap(i, j + 1)
+
+print(t)
