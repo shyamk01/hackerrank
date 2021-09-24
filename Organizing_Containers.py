718a659e8cccb940d0df7ddbd8f9c8c698ddaee1
shyam kumar <shyam.cs.engg@gmail.com>
2021-09-24 21:16:19 +0530

    https://www.hackerrank.com/challenges/organizing-containers-of-balls/problem Solutions

--- Organizing_Containers.py
+++ Organizing_Containers.py
@@ -1,27 +1,18 @@
 t = [[0, 2, 1], [1, 1, 1], [2, 0, 0]]
 
+t1 = []
+t2 = []
 
-# 0 ->3
-# 1->4
-# 2->2
+for i in range(len(t)):
+    s = 0
+    s1=0
+    for j in range(len(t)):
+        s = s + (t[i][j])
+        s1 = s1 + (t[j][i])
+    t1.append(s)
+    t2.append(s1)
 
-def matrixswap(i, j):
-    while (t[i][j] != 0) or (t[j][i] != 0) or (t[i + 1][j] != 0) or (t[j + 1][i] != 0):
-        t[i][i] = t[i][i] + 1
-        t[i][j] = t[i][j] - 1
-
-        t[j][i] = t[j][i] - 1
-        t[j][j] = t[j][j] + 1
-
-        print(i, j, t)
-        if (t[i][j] == 0) or (t[j][i] == 0) or (t[i + 1][j] == 0) or (t[j + 1][i] == 0):
-            break
-
-
-for i in range(len(t) - 1):
-    for j in range(len(t) - 1):
-        if i != j + 1:
-            print(i, j + 1)
-            matrixswap(i, j + 1)
-
-print(t)
+if sorted(t1)==sorted(t2):
+    print("Possible")
+else:
+    print("Impossible")
