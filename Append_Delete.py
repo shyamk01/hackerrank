52e07e85b2fb1d661212c401b734a806f41669c5
shyam kumar <shyam.cs.engg@gmail.com>
2021-09-30 14:48:05 +0530

    https://www.hackerrank.com/test/ch1k00qp1cn/questions/99f8cik52st
    revesed swap function

--- Append_Delete.py
+++ Append_Delete.py
@@ -1,21 +1,17 @@
-s="abcd"
-t="abcdert"
-k=10
-s=list(s)
-t=list(t)
-l=min(len(s),len(t))
+import os
 
+s = "hackerhappy"
+t = "hackerrank"
+k = 10
 
-for i in range(l):
-    if s[i]!=t[i]:
-        s[i]=-1
-        t[i]=-1
-    else:
-        break
-s = [x for x in s if x != -1]
-t = [x for x in t if x != -1]
-
-if len(s)+len(t)<=k :
-    print("Yes")
-else:
+prefix = os.path.commonprefix([s, t])
+p = len(prefix)
+d = len(s) - p
+a = len(t) - p
+m = a + d
+e = k - m
+if e < 0:
     print("No")
+if e < 2 * p and e % 2 != 0:
+    print("No")
+print("Yes")
