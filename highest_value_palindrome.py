0a4fb5fed763f4b7199b213319b98fa825de1d1a
shyamk01 <shyam.cs.engg@gmail.com>
2022-05-31 22:27:49 +0530

    https://www.hackerrank.com/challenges/richie-rich/problem 33/33 test cases passed

--- highest_value_palindrome.py
+++ highest_value_palindrome.py
@@ -1,50 +1,47 @@
-s="777"
-k=0
-lenk=len(s)//2
-arrfront=""
-arrrev=""
-arrfinal=[0]*len(s)
-flag=False
-arrfront=s[0:lenk]
-if len(s)%2==0:
-    t=s[lenk:len(s)]
-    arrrev = t[::-1]
-else:
-    flag=True
-    arrmiddle =s[len(s)%2]
-    t = s[lenk+1:len(s)]
-    arrrev=t[::-1]
-i=0
-while(i<len(arrrev)):
-        if k==0:
-            break
-        if k == 1:
-            if arrfront[i]==arrrev[i]:
-                pass
-            else:
-                maxval=max(arrrev[i],arrfront[i])
-                arrfront = arrfront[:i] + maxval + arrfront[i + 1:]
-                arrrev = arrrev[:i] + maxval + arrrev[i + 1:]
-                k=k-1
-
+def highestValuePalindrome(s, n, k):
+    j=0
+    v=n-1
+    c=0
+    s=[int(v) for v in s]
+    a=[0]*n
+    while j<n//2:
+        # If the value of the front and back is not equal then set the value of max in the front one
+        if s[j]!=s[v]:
+            c=c+1
+            s[j]=max(s[j],s[v])
+            s[v]=s[j]
+            a[j]=1
+        j=j+1
+        v=v-1
+        # If the count of the change's index value of the array is greater then the what the value provided by input,
+        # then we the string is not palindrome
+        if c>k:
+            return '-1'
+    if c<k:
+        m=k-c
+        l=n//2
+        for j in range(l):
+            if a[j]==1:
+                if s[j]!=9:
+                    m=m-1
+                    s[j]=9
+            elif a[j]==0 and m>=2:
+                if s[j]!=9:
+                    m=m-2
+                    s[j]=9
+            if m==0:
+                break
+        print(s,a)
+        # check the string length is not integer and still have the value(m), to replace with maximum digit
+        if n%2!=0:
+            if m>0:
+                s[n//2]=9
+            s=s[:l+1]+s[:l][::-1]
         else:
-            if arrfront[i]==arrrev[i]=="9":
-                pass
-            else:
-                maxval = "9"
-                arrfront = arrfront[:i] + maxval + arrfront[i + 1:]
-                arrrev = arrrev[:i] + maxval + arrrev[i + 1:]
-                k=k-2
+            s=s[:l]+s[:l][::-1]
+        print(s)
+    return "".join(map(str,s))
 
-        i=i+1
-final=""
-if k==0:
-    final=s
-if flag==True &k==1:
-    final=arrfront+"9"+arrrev[::-1]
-else:
-    final=arrfront+arrrev[::-1]
-if final==final[::-1]:
-    print(final)
-else:
-    print(-1)
+s="99341"
+
+print(highestValuePalindrome("3943", 4, 4))
